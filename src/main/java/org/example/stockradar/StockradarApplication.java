fc0da77086e8bba1605a03d9cc66b04b398a6dc5
Geunyeong Kim <98966788+Dubutoto@users.noreply.github.com>
2025-02-20 15:43:20 +0900

    Initial commit

---
+++ src/main/java/org/example/stockradar/StockradarApplication.java
@@ -0,0 +1,13 @@
+package org.example.stockradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockradarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockradarApplication.class, args);
+    }
+
+}
