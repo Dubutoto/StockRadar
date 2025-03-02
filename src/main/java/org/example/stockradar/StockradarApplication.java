7b4a215884ebd554f1378a08511eb0d8ac6942fd
Hyun7en <rhqudgus99@gmail.com>
2025-03-02 22:35:44 +0900

    chore: .gitignore, config 및 StockradarApplication 파일 수정 (#36)

--- src/main/java/org/example/stockradar/StockradarApplication.java
+++ src/main/java/org/example/stockradar/StockradarApplication.java
@@ -2,8 +2,6 @@ package org.example.stockradar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class StockradarApplication {
@@ -12,8 +10,4 @@ public class StockradarApplication {
         SpringApplication.run(StockradarApplication.class, args);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void onApplicationReady() {
-        System.out.println("서버주소: http://localhost:8080");
-    }
 }
