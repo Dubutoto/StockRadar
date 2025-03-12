f832b1b9e123990478ba9e5a4301c3a490041a5c
Byeonghyun Go <rhqudgus99@gmail.com>
2025-03-12 21:53:31 +0900

    feat: 알림 controller 추가 (#127)

---
+++ src/main/java/org/example/stockradar/feature/notification/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package org.example.stockradar.feature.notification.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author
+ */
+
+@RestController
+@RequestMapping("notification")
+@RequiredArgsConstructor
+public class NotificationController {
+}
