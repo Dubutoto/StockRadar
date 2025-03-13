b4f334fa56223bd23bb382dcd11fe5c91d8a3a61
jin_5 <krt8599@naver.com>
2025-03-13 16:49:04 +0900

    refactor: 시나리오4번 V2구현완료(#134)

--- src/main/java/org/example/stockradar/feature/product/rtx4060Ti/controller/Rtx4060Ticontroller.java
+++ src/main/java/org/example/stockradar/feature/product/rtx4060Ti/controller/Rtx4060Ticontroller.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.stockradar.feature.crawl.service.ProductStockService;
 import org.example.stockradar.feature.product.dto.ProductResponseDto;
-import org.example.stockradar.feature.product.rtx4060Ti.service.Rtx4060TiAllCacheService;
-import org.example.stockradar.feature.product.rtx4060Ti.service.Rtx4060TiService;
-import org.example.stockradar.feature.product.rtx4060Ti.service.Rtx4060TiStockCacheService;
-import org.example.stockradar.feature.product.rtx4060Ti.service.Rtx4060TiStockCacheTestService;
+import org.example.stockradar.feature.product.rtx4060Ti.service.*;
 import org.example.stockradar.global.exception.ErrorCode;
 import org.example.stockradar.global.exception.specific.ProductException;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +25,8 @@ public class Rtx4060Ticontroller {
     private final Rtx4060TiService rtx4060TiService;
     private final Rtx4060TiStockCacheService rtx4060TiStockCacheService; //시나리오3 서비스
     private final Rtx4060TiStockCacheTestService rtx4060TiStockCacheTestService;//시나리오3테스트 서비스
-    private final Rtx4060TiAllCacheService rtx4060TiAllCacheService;//시나리오 4 테스트 서비스
+    //private final Rtx4060TiAllCacheService rtx4060TiAllCacheService;//시나리오 4 테스트 서비스
+    private final Rtx4060TiAllCacheServiceV2 rtx4060TiAllCacheServiceV2;//시나리오 4 테스트 서비스v2
 
 
     @GetMapping("gpu/rtx4060Ti")
@@ -122,7 +120,7 @@ public class Rtx4060Ticontroller {
             //List<ProductResponseDto> allProducts = rtx4060TiStockCacheTestService.getRtx4060TiInfo(); //캐시 테스트 서비스 시나리오3
 
 
-            List<ProductResponseDto> allProducts = rtx4060TiAllCacheService.getAllProducts();   // 모든 제품 정보 가져오기 (캐시 활용)시나리오4
+            List<ProductResponseDto> allProducts = rtx4060TiAllCacheServiceV2.getAllProducts();   // 모든 제품 정보 가져오기 (캐시 활용)시나리오4
 
             // RTX 4060Ti 제품만 필터링
             List<ProductResponseDto> rtx4060TiProducts = allProducts.stream()
