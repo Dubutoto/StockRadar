f35f589ba7452098d8815fc3ca8835a29a690006
Byeonghyun Go <rhqudgus99@gmail.com>
2025-03-14 18:27:57 +0900

    feat: 알림 기능 구현 1차 (#127)

---
+++ src/main/java/org/example/stockradar/config/JdaConfig.java
@@ -0,0 +1,23 @@
+package org.example.stockradar.config;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.security.auth.login.LoginException;
+
+@Configuration
+public class JdaConfig {
+
+    @Value("${discord.bot.token}")
+    private String botToken;
+
+    @Bean
+    public JDA jda() throws LoginException, InterruptedException {
+        return JDABuilder.createDefault(botToken)
+                .build()
+                .awaitReady();
+    }
+}
