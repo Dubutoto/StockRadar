f6601367274fff8ee89ad488478bf0c2156636db
Geunyeong Kim <98966788+Dubutoto@users.noreply.github.com>
2025-03-17 11:36:20 +0900

    bugfix: 사라진 회원가입 버튼 되찾기

--- crawlingServer/coroutine.py
+++ crawlingServer/coroutine.py
@@ -110,7 +110,7 @@ async def main():
         host="localhost",
         port=3306,
         user="root",
-        password="1234",
+        password="Dubutoto22!",
         db="stockradar",
         autocommit=True,
         minsize=1,
